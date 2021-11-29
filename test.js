8f3106019b513c9d16e0c057fe809ac12d4eedd9
Mathieu <drapalamathieu95@gmail.com>
2021-11-29 21:41:53 +0100

    feat: add exercise-0 and test

---
+++ test.js
@@ -0,0 +1,15 @@
+import { assert } from "chai";
+import { my_sum } from "./day-1/exercise-0.js";
+
+describe("Test day-1", function () {
+  //Exercise 0
+  describe("exercise-0", function () {
+    it("Should know how to add up", function () {
+      let cal1 = 1;
+      let cal2 = 2;
+      const res = my_sum(1, 2);
+
+      assert.equal(res, 0 || cal1 + cal2);
+    });
+  });
+});
